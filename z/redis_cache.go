dfc795c272afeea0b3ef570e8b7f71c8663de01b
Bobby <ziee@qq.com>
2025-02-05 15:22:09 +0800

    add functions

---
+++ z/redis_cache.go
@@ -0,0 +1,103 @@
+package z
+
+import (
+	"context"
+	"fmt"
+	"github.com/goccy/go-json"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+type redisCache struct {
+	client *redis.Client
+}
+
+var RedisCache redisCache
+
+// New 初始化 redis
+func (r *redisCache) New() {
+	host, _ := Config.String("config.redis.host")
+	port, _ := Config.Int("config.redis.port")
+	password, _ := Config.String("config.redis.password")
+	db, _ := Config.Int("config.redis.db")
+
+	r.client = redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", host, port),
+		Password: password,
+		DB:       db,
+	})
+
+	ctx := context.Background()
+
+	if _, err := r.client.Ping(ctx).Result(); err != nil {
+		Error.Fatal("redis connect error: ", err.Error())
+	}
+}
+
+// Get 获取 key 的值
+func (r *redisCache) Get(key string, dest *interface{}) error {
+	ctx := context.Background()
+	res, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal([]byte(res), dest)
+}
+
+// Set 设置 key 的值
+func (r *redisCache) Set(key string, value interface{}, duration time.Duration) error {
+	ctx := context.Background()
+
+	jsonValue, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+
+	return r.client.Set(ctx, key, jsonValue, duration).Err()
+}
+
+// Exists 判断 key 是否存在
+func (r *redisCache) Exists(key string) bool {
+	ctx := context.Background()
+
+	if exists, err := r.client.Exists(ctx, key).Result(); 0 == exists || err != nil {
+		return false
+	}
+
+	return true
+}
+
+// Delete 删除 key
+func (r *redisCache) Delete(key string) error {
+	ctx := context.Background()
+
+	if err := r.client.Del(ctx, key).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Expire 设置 key 的过期时间
+func (r *redisCache) Expire(key string, duration time.Duration) error {
+	ctx := context.Background()
+
+	if err := r.client.Expire(ctx, key, duration).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TTL 获取 key 剩余的时间
+func (r *redisCache) TTL(key string) (time.Duration, error) {
+	ctx := context.Background()
+
+	duration, err := r.client.TTL(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	return duration, nil
+}
