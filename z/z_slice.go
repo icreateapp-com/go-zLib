a7f2f52d30adacb2b0e02e3bffb93035a16ed435
duncanxia <duncanxia97@gmail.com>
2025-05-24 23:39:37 +0800

    切片去重

--- z/z_slice.go
+++ z/z_slice.go
@@ -105,3 +105,16 @@ func HasFieldInMap(value interface{}, field string) bool {
 	_, exists := m[field]
 	return exists
 }
+
+// SliceUnique 删除切片中的重复元素
+func SliceUnique[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	var result []T
+	for _, val := range slice {
+		if _, ok := seen[val]; !ok {
+			seen[val] = true
+			result = append(result, val)
+		}
+	}
+	return result
+}
